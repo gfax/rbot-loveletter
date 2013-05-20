d2c024aa9b2cfe3f128b392f39dde7d327fbc8ec
Jay Thomas <degradinglight@gmail.com>
2013-05-20 02:48:16 -0400

    backup

--- loveletter.rb
+++ loveletter.rb
@@ -9,6 +9,8 @@ class LoveLetter
 
   Title = Irc.color(:red) + 'Love Letter'
 
+  Rounds = Struct.new(:played, :total)
+
   Cards = {
     :guard => {
       :value => 1,
@@ -77,7 +79,9 @@ class LoveLetter
     end
 
     def to_s
-      Bold + name.to_s.capitalize + value.to_s + Bold
+      Irc.color(:red) + '[' + NormalText +
+      value.to_s + '. 'name.to_s.capitalize +
+      Irc.color(:red) + ']' + NormalText
     end
 
   end
@@ -85,13 +89,26 @@ class LoveLetter
 
   class Player
 
-    attr_accessor :user, :discard, :hand, :out, :time
+    attr_accessor :user, :discard, :hand, :moved, :out, :time
 
     def initialize(user)
       @user = user
       @discard = []
       @hand = []
+      @moved = false
       @out = false
+    end
+
+    def has?(requests)
+      return false if requests.nil?
+      [*requests].each do |e|
+        if e.is_a? Card
+          hand.each { |c| return true if e == c }
+        elsif e.is_a? Symbol
+          hand.each { |c| return true if e == c.id }
+        end
+      end
+      return false
     end
 
     def to_s
@@ -99,6 +116,8 @@ class LoveLetter
     end
   end
 
+  attr_reader :deck, :discard, :dropped, :manager,
+              :players, :queue, :rounds, :started
 
   def initialize(plugin, channel, user, rounds)
     @bot = plugin.bot
@@ -106,12 +125,14 @@ class LoveLetter
     @plugin = plugin
     @registry = plugin.registry
     @deck = []        # card stock
-    @dropped = []     # players booted from the game
+    @discard = nil    # current discard
+    @dropped = []     # players booted from game
     @join_timer = nil # timer for countdown
-    @manager = nil    # player that started the game
-    @player = []      # players currently in the game
+    @manager = nil    # player in control of game
+    @players = []     # players currently in game
+    @queue = nil      # temporary message queue
     @reserve = []     # card reserve for round end
-    @rounds = rounds  # total rounds in the game
+    @rounds = Rounds.new(0, rounds)  # rounds in game
     @started = nil    # time the game started
     add_player(user)
   end
@@ -119,6 +140,9 @@ class LoveLetter
   def add_player(user)
     if player = get_player(user)
       say "You're already in the game #{player}."
+      return
+    elsif players.size  > 3
+      say "Sorry, this game can only seat 4 players at a time."
       return
     elsif deck.size < 2
       say "Round is about to end. Wait until next round to join, #{user}."
@@ -133,7 +157,12 @@ class LoveLetter
     end
     player.hand << draw
     if @join_timer
-      @bot.timer.reschedule(@join_timer, 10)
+      if players.size == 4
+        @bot.timer.remove(@join_timer)
+        do_round
+      else
+        @bot.timer.reschedule(@join_timer, 10)
+      end
     elsif players.size > 1
       countdown = @bot.config['loveletter.countdown']
       @join_timer = @bot.timer.add_once(countdown) { do_round }
@@ -141,7 +170,53 @@ class LoveLetter
     end
   end
 
-  def do_round
+  def do_discard(player, a)
+    card = opponent = guard_guess = nil
+    a.each do |e|
+      case e
+      when Fixnum
+        card = player.hand[e] if e >= 0 and card.nil?
+      when Player
+        opponent = e if opponent.nil?
+      when Card
+        card = e if card.nil? and player.has?(e)
+        guard_guess = e if guard_guess.nil?
+      end
+    end
+    if card.nil?
+      notify player, "Specify a card name or number."
+      return false
+    end
+    @discard = card
+    player.discard << card
+    player.hand.delete(card)
+    @queue = "#{player} plays #{card}"
+    case card.id
+    when :guard
+      return do_guard([opponent, guard_guess]) if opponent and guard_guess
+    when :priest
+      return do_priest([opponent]) if opponent
+    when :baron
+      return do_baron([opponent]) if opponent
+    when :handmaid
+      @queue << " and is now immune until next turn."
+      return true
+    when :prince
+      return do_prince([opponent]) if opponent
+    when :king
+      return do_king([opponent]) if opponent
+    when :countess
+      @queue << '.'
+      return true
+    when :princess
+      @queue << " and is knocked out of the round."
+      player.out = true
+      return true
+    end
+    return false
+  end
+
+  def do_round(last_winner=nil)
     @started = Time.now if not started
     @players.shuffle!
     # Reset deck:
@@ -151,9 +226,30 @@ class LoveLetter
       v[:quantity].times { @deck << Card.new(k) }
     end
     players.each { |p| p.hand << @deck.pop }
+    if players.size == 2
+      @reserve |= @deck.pop(4)
+    else
+      @reserve << @deck.pop
+    end
+    if last_winner
+      players.size.times do
+        if players[1] == last_winner
+          break
+        else
+          @players << @players.shift
+        end
+      end
+    end
+    do_turn
   end
 
   def do_turn(hold_place=false)
+    done = true
+    players.each { |p| done = false unless player.moved }
+    if done
+      end_round
+      return
+    end
     @players << @players.shift unless hold_place
     player = players.first
     while player.hand.size < 2
@@ -220,7 +316,16 @@ class LoveLetter
   end
 
   def end_round
-    
+    @rounds.played += 1
+  end
+
+  def get_card(cards)
+    [*cards].each do |e|
+      Cards.each_pair do |k,v|
+        return Card.new(k) if e.gsub('.','') =~ v[:keyword]
+      end
+    end
+    return nil
   end
 
   def get_player(user, source=nil)
@@ -237,7 +342,7 @@ class LoveLetter
       end
       players.each do |p|
         if p.user.irc_downcase =~ /^#{user.irc_downcase(channel.casemap)}/
-          return p unless p.user.irc_downcase == source
+          return p unless p.user.irc_downcase == source.to_s.downcase
         end
       end
     else
@@ -248,6 +353,26 @@ class LoveLetter
 
   def notify(player, msg, opts={})
     @bot.notice player.user, msg, opts
+  end
+
+  def processor(player, a)
+    return unless player == players.first
+    return if player.moved or a.empty?
+    player.moved = true
+    a.map! do |e|
+      get_card(e) || get_player(e, player.user) || e.to_i - 1
+    end
+    player.moved = if discard
+      self.send("do_#{discard.id}", player, a)
+    else
+      do_discard(player, a)
+    end
+    if player.moved
+      do_turn
+    else
+      say queue
+      @queue = nil
+    end
   end
 
   def replace_player(replacer, a)
@@ -292,12 +417,24 @@ class LoveLetter
     @bot.say who, msg, opts
   end
 
+  def show_help(player, a)
+    return unless a.first.to_i.between?(1, player.hand.size)
+    card = player.hand[a.first.to_i-1]
+    notify player, "#{card} - #{Cards[card.id[:text]}"
+  end
+
   def show_hand(p_array=players)
-    p_array = [ p_array ] unless p_array.class == Array
-    p_array.each do |p|
+    [*p_array].each do |p|
       next if p.hand.size < 1
       notify p, "Cards: 1.) #{p.hand[0]}, 2.) #{p.hand[1]}"
     end
+  end
+
+  def show_turn
+    return unless started
+    string = "It's #{players.first}'s turn."
+    string << " Current discard: #{discard}" if discard
+    say string
   end
 
   def transfer_management(player, a)
@@ -307,12 +444,11 @@ class LoveLetter
                      "#{manager} manages this game."
       return
     end
-    [ 'game', 'manager', 'management', 'ownership', 'to' ].each do |w|
-      a.delete_at(0) if a.first == w
+    a.each do |e|
+      break if new_manager = get_player(e, manager.user)
     end
-    new_manager = get_player(a.first, manager.user.downcase)
     if new_manager.nil?
-      say "'#{a.first}' is not playing #{Title}"
+      say "#{player}: Specify another player."
       return
     elsif manager == new_manager
       say "#{player.user}: You are already game manager."
@@ -325,15 +461,13 @@ class LoveLetter
   def update_channel_stats(stats)
     r = @registry[:chan] || {}
     c = channel.name.downcase
-    rounds = 0
-    players.each { |p| rounds += p.rounds }
     r[c] = {} if r[c].nil?
     r[c][:games] = r[c][:games].to_i + 1
     r[c][:longest] = started if r[c][:longest].nil?
     r[c][:longest] = started if started > r[c][:longest]
     # display-name for proper caps
     r[c][:name] = channel.name
-    r[c][:rounds] = r[c][:rounds].to_i + rounds
+    r[c][:rounds] = r[c][:rounds].to_i + rounds.total
     r[c][:time] = r[c][:time].to_i + started
     @registry[:chan] = r
   end
@@ -348,6 +482,7 @@ class LoveLetter
       # Get player's nick in proper caps.
       e[:nick] = player.user.to_s
       e[:rounds] = e[:rounds].to_i + player.rounds
+      e[:rounds] += (rounds.total * win)
       e[:wins] = e[:wins].to_i + win
     end
     r1 = @registry[:chan]
@@ -405,28 +540,26 @@ class LoveLetterPlugin < Plugin
       "round.\nIf all other players are protected by the " +
       "Handmaid, you must choose yourself."
     when /maid/
-      "#{Bold}Handmaid Susannah #{Bold}- When you discard the Handmaid, " +
-      "you are immune to the effects of other players' cards until the " +
-      "start of your next turn, If all other players other than the " +
-      "player whose turn it is are protected by Susannah, the player " +
-      "must choose him, or herself, if possible."
+      "#{Bold}Handmaid Susannah #{Bold}- When you discard " +
+      'the Handmaid, you are immune to the effects of other ' +
+      'players\' cards until the start of your next turn.'
     when /baron/
       "#{Bold}Baron Talus #{Bold}- When discarded, choose one other " +
-      "player still in the round. You andthat player secretly compare " +
-      "your hands. The player with the lower rank is knocked out of the " +
-      "round. In case of a tie, nothing happens. If all other players " +
-      "still in the round are protected by the Handmaid, " +
-      "this card does nothing."
+      'player still in the round. You andthat player secretly compare ' +
+      'your hands. The player with the lower rank is knocked out of the ' +
+      'round. In case of a tie, nothing happens. If all other players ' +
+      'still in the round are protected by the Handmaid, ' +
+      'this card does nothing.'
     when /priest/
       "#{Bold}Priest Tomas #{Bold}- When you discard the " +
       "Priest, you can look at one other player’s hand. " +
-      "Do not reveal the hand to all players."
+      'Do not reveal the hand to all players.'
     when /guard/
       "#{Bold}Guard Odette #{Bold}- When you discard the Guard, " +
-      "choose a player and name a card (other than Guard). If " +
-      "that player has that card, that player is knocked out " +
-      "of the round. If all other players still in the round " +
-      "are protected by the Handmaid, this card does nothing."
+      'choose a player and name a card (other than Guard). If ' +
+      'that player has that card, that player is knocked out ' +
+      'of the round. If all other players still in the round ' +
+      'are protected by the Handmaid, this card does nothing.'
     when /manage/, /transfer/, /xfer/
       'The player that starts the game is the game manager. ' +
       'Game managers may stop the game at any time, or transfer ownership ' +
@@ -440,9 +573,16 @@ class LoveLetterPlugin < Plugin
       "love letter at the end of the day, so it reaches her first!"
     when /rule/, /manual/
       "http://www.alderac.com/tempest/files/2012/09/Love_Letter_Rules_Final.pdf"
+    when /scoring/
+      'The winner of each round receives a token of affection ' +
+      'from the princess. The player that received the most affection ' +
+      'by the end of all the rounds is declared the winner. Players\' ' +
+      'scores are updated relative to the amount of affection won. ' +
+      'The winner over all rounds receives a bonus relative ' +
+      'to the total number of rounds played.'
     when /stat/, /scor/
       "'#{p}#{plugin} stats <channel|user>' displays the stats " +
-      "and scores for a channel or user. If no channel or user " +
+      'and scores for a channel or user. If no channel or user ' +
       "is specified, this command will show you your own stats.\n" +
       "'#{p}#{plugin} stats <channel> <user>' displays user " +
       "stats for a specific channel\n'#{p}#{plugin} top <num> " +
@@ -451,7 +591,7 @@ class LoveLetterPlugin < Plugin
       "'#{p}#{plugin} stop' stops the current game; Only game " +
       'managers and bot owners can stop a game in progress.'
     when ''
-      "#{Title}: commands, manual, object, stats, stop -- " +
+      "#{Title}: commands, manual, object, scoring, stats, stop -- " +
       "'#{p}#{plugin} <rounds>' to create a game"
     end
   end
@@ -465,6 +605,40 @@ class LoveLetterPlugin < Plugin
       end
     else
       @games[m.channel] = LoveLetter.new(self, m.channel, m.source)
+    end
+  end
+
+  def message(m)
+    return unless m.plugin and g = @games[m.channel]
+    case m.message.downcase
+    when 'j', 'jo', 'join'
+      g.add_player(m.source)
+    when 'ti', 'time'
+      if g.started
+        @bot.say m.replyto, Title + " has been in play for #{g.elapsed_time}."
+      else
+        m.reply Title + " hasn't started yet."
+      end
+    end
+    # Messages only concerning players:
+    player = g.get_player(m.source.nick)
+    return unless player and g.started
+    a = m.message.downcase.split(' ').uniq[1..-1]
+    case m.message.downcase
+    when /^(ca?|cards?)( |\z)/
+      g.show_hand(player)
+    when /^drop( |\z)/
+      g.drop_player(player, a)
+    when /^h(elp)?( |\z)/
+      g.show_help(player, a)
+    when /^(pi?|pl|play)( |\z)/
+      g.processor(player, a)
+    when /^(tu?|turn)( |\z)/
+      g.show_turn
+    when /^replace( |\z)/
+      g.replace_player(player, a)
+    when /^transfer( |\z)/
+      g.transfer_management(player, a)
     end
   end
 
